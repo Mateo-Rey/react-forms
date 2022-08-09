daf9caeaed6bf449a52d928b3c9ab36030a09a1f
Mateo-Rey <jrey3darts@gmail.com>
2022-08-09 15:32:00 -0400

    form setup and linked to api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+  const [title, setTitle] = useState("this is the title");
+  const [description, setDescription] = useState("your description");
+  const [author, setAuthor] = useState("other");
+
+  async function formSubmit(e) {
+    try{
+    e.preventDefault();
+
+    const comment = {
+      title,
+      description,
+      author,
+    };
+    console.log(comment);
+    const results = await fetch("https://sql.bocacode.com/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(comment),
+    });
+    const data = results.json();
+    console.log(data);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+        <label>Title</label>
+        <input
+          type="type"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <h3>{title}</h3>
+        <label>Description</label>
+        <textarea
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        ></textarea>
+        <h3>{description}</h3>
+
+        <label>Author</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="">Choose One</option>
+          <option value="todd">Doctor Todd</option>
+          <option value="ludwigson">Ludwigson</option>
+          <option value="other">Other</option>
+        </select>
+        <h3>{author}</h3>
+
+        <button>Submit Form</button>
+      </form>
+    </>
   );
 }
 
